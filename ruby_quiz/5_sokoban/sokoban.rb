794d98580674a9e3846027c2b1dfead40f2de700
Jeremy Wilmot <j1wilmot@gmail.com>
2012-12-17 23:15:20 -0500

    Add work with sokoban

--- ruby_quiz/5_sokoban/sokoban.rb
+++ ruby_quiz/5_sokoban/sokoban.rb
@@ -1,4 +1,58 @@
 class Sokoban
 
+  def initialize
+    @victory = false
+    @level = Level.new('level_test.txt')
+  end
+  
+  def victory?
+    @victory
+  end
 
+  def to_s
+    puts @level.to_s
+  end
+end
+
+class Level
+  MAN = '@'
+  CRATE = 'o'
+  WALL = '#'
+  OPEN_FLOOR = ' '
+  STORAGE = '.'
+  CRATE_ON_STORAGE = '*'
+  MAN_ON_STORAGE = '+'
+
+  def initialize(filename)
+    @board = []
+    File.open(filename) do |file|
+      file.lines do |line|
+        @board[@board.size] = line
+      end
+    end
+    @man_position = find_position()
+  end
+
+  def find_position()
+    #Position.new(0, 0)
+    false
+  end
+
+  def to_s
+    @board.join
+  end
+end
+
+class Position
+  attr_accessor :x, :y
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+
+  def ==(other)
+    other.x == x && other.y == y
+  end
+    
 end
--- ruby_quiz/5_sokoban/sokoban_spec.rb
+++ ruby_quiz/5_sokoban/sokoban_spec.rb
@@ -1,8 +1,26 @@
 require './sokoban.rb'
+require 'minitest/autorun'
 
-describe Sokoban, "#test" do
-	it "returns 0" do
-		sokoban = Sokoban.new
-		sokoban.test.should eq(0)
-	end
+
+class TestLevel < MiniTest::Unit::TestCase
+
+  def test_load_level
+    level_text = "#####\n" +
+           "#.o@#\n" +
+           "#####"
+    level = Level.new('level_test.txt')
+    puts level_text, level.to_s
+    assert_equal level_text, level.to_s 
+    position = Position.new(1, 3)
+    assert_equal position, level.find_position
+  end
+end
+
+class TestSokoban < MiniTest::Unit::TestCase
+
+  def test_that_board_starts_in_non_wining_state
+    game = Sokoban.new
+    assert !game.victory?
+  end
+
 end
